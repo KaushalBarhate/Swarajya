1a2f63b0d7422105890eaa8cb3ce90ae7e3999c7
Siddhesh Kushare <72686297+Siddhesh21-coder@users.noreply.github.com>
2023-02-18 16:12:53 +0530

    Update Carousel.js

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -53,7 +53,7 @@ const properties = {
 };
 const Slideshow = () => { 
   return (
-    <div className="slide-container mx-auto w-3/4 md:mx-auto w-1/2 ">
+    <div className="slide-container mx-auto sm:w-1/2 md:mx-auto w-3/4 ">
       <Slide {...properties}>
         {slideImages.map((slideImage, index) => (
           <div key={index}>
